e9c7050ab6e1f5edbb374f164dbe33f58e236c7d
DESKTOP-8R4KCSO\alwjw <alwjw@naver.com>
2024-12-29 18:46:31 +0900

    no message

---
+++ Maze-array/Maze/Maze.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include "Array.h"
+#include "Vector.h"
+
+using namespace std;
+
+int main()
+{
+	Vector v;
+
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(3);
+
+	int a = v[1];
+
+}
